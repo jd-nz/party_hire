37370825e42eb368cf2726af5421b448b3f22a38
jd-nz <jason.duong77@gmail.com>
2021-09-28 22:51:03 +1300

    firstname and surname

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ from numpy import disp
 import pandas as pd
 from pandastable import Table
 
-data = {"full_name": [],
-            "receipt_no": [],
-            "item_hired": [],
-            "quantity": []}
+data = {"first_name": [],
+        "surname": [],
+        "receipt_no": [],
+        "item_hired": [],
+        "quantity": []}
 
-def enter_data(full_name, receipt_no, item_hired, quantity):
-    data["full_name"].append(full_name.get())
+def enter_data(first_name, surname, receipt_no, item_hired, quantity):
+    data["first_name"].append(first_name.get())
+    data["surname"].append(surname.get())
     data["receipt_no"].append(receipt_no.get())
     data["item_hired"].append(item_hired.get())
     data["quantity"].append(quantity.get())
@@ -22,7 +24,7 @@ class view_data(Frame):
         self.main = self.master
         self.main.title('Table')
         f = Toplevel(self.main)
-        df = pd.DataFrame(data, columns = ["full_name", "receipt_no", "item_hired", "quantity"])
+        df = pd.DataFrame(data, columns = ["first_name", "surname", "receipt_no", "item_hired", "quantity"])
         self.table = pt = Table(f, dataframe=df,
                                 showtoolbar=False, showstatusbar=False)
         pt.show()
@@ -43,39 +45,44 @@ def gui():
 
     #Create
 
-    #Full Name
-    tk.Label(master, text="Full Name").grid(row=1)
-    full_name_value = tk.StringVar()
-    full_name = tk.Entry(master, textvariable=full_name_value).grid(row=1, column=1)
+    #First Name
+    tk.Label(master, text="First Name").grid(row=1)
+    first_name_value = tk.StringVar()
+    first_name = tk.Entry(master, textvariable=first_name_value).grid(row=1, column=1)
+
+    #Surname
+    tk.Label(master, text="Surname").grid(row=2)
+    surname_value = tk.StringVar()
+    surname = tk.Entry(master, textvariable=surname_value).grid(row=2, column=1)
 
     #Receipt No.
-    tk.Label(master, text="Receipt No.").grid(row=2)
+    tk.Label(master, text="Receipt No.").grid(row=3)
     receipt_no_value = tk.StringVar()
-    receipt_no = tk.Entry(master, textvariable=receipt_no_value).grid(row=2, column=1)
+    receipt_no = tk.Entry(master, textvariable=receipt_no_value).grid(row=3, column=1)
 
     #Item Hired
-    tk.Label(master, text="Item Hired").grid(row=3)
+    tk.Label(master, text="Item Hired").grid(row=4)
     item_hired_value = tk.StringVar()
-    item_hired = tk.Entry(master, textvariable=item_hired_value).grid(row=3, column=1)
+    item_hired = tk.Entry(master, textvariable=item_hired_value).grid(row=4, column=1)
 
     #Quantity
-    tk.Label(master, text="Quantity").grid(row=4)
+    tk.Label(master, text="Quantity").grid(row=5)
     quantity_value = tk.StringVar()
-    quantity = tk.Entry(master, textvariable=quantity_value).grid(row=4, column=1)
+    quantity = tk.Entry(master, textvariable=quantity_value).grid(row=5, column=1)
 
     #Commands
-    command_enter_data = partial(enter_data, full_name_value, receipt_no_value, item_hired_value, quantity_value)
+    command_enter_data = partial(enter_data, first_name_value, surname_value, receipt_no_value, item_hired_value, quantity_value)
 
     #Buttons
-    enter_data_button = tk.Button(master, text="Enter Data", command=command_enter_data).grid(row=4, column=2)
-    view_data_button = tk.Button(master, text="View Data", command=view_data).grid(row=5, column=1)
+    enter_data_button = tk.Button(master, text="Enter Data", command=command_enter_data).grid(row=5, column=2)
+    view_data_button = tk.Button(master, text="View Data", command=view_data).grid(row=6, column=1)
 
     #Delete
-    tk.Label(master, text="Delete Data").grid(row=6, column=1)
-    tk.Label(master, text="Data Row").grid(row=7, column=0)
+    tk.Label(master, text="Delete Data").grid(row=7, column=1)
+    tk.Label(master, text="Data Row").grid(row=8, column=0)
     delete_row_value = tk.StringVar()
-    delete_row = tk.Entry(master, textvariable=delete_row_value).grid(row=7, column=1)
-    delete_row_button = tk.Button(master, text="Delete Data").grid(row=8, column=1)
+    delete_row = tk.Entry(master, textvariable=delete_row_value).grid(row=8, column=1)
+    delete_row_button = tk.Button(master, text="Delete Data").grid(row=9, column=1)
 
     master.mainloop()
 
